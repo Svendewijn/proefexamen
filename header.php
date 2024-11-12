25db1629172c7ac2634fac1cdff69f49f85df0db
Svendewijn <sven11042006@gmail.com>
2024-11-12 10:46:27 +0100

    header

---
+++ header.php
@@ -0,0 +1,7 @@
+<div class="header">
+    <a href="#home">Bestand</> |
+    <a href="#over">Bekijk</a> |
+    <a href="#contact">Help</a>
+    <a href="inlog.php">Login</a>
+
+</div>
--- index.php
+++ index.php
@@ -4,20 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welkom</title>
-    <link rel="stylesheet" href="styling.css">
+    <link rel="stylesheet" href="css/styling.css">
 </head>
 <body>
+<?php include 'header.php';?>
     <div class="welkom">
         <h1>Welkom</h1>
-        <div class="welkom-text">
-            <p>
+         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        </div>
     </div>
 </body>
 </html>
