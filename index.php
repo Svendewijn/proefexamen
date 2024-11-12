97fda197b62f4379ed173a1d62f7ec03952a6558
Svendewijn <sven11042006@gmail.com>
2024-11-12 11:51:43 +0100

    footer toegevoegd

--- index.php
+++ index.php
@@ -18,5 +18,6 @@
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
     </div>
+    <?php include 'footer.php';?>
 </body>
 </html>
