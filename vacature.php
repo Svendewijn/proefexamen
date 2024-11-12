c54fc2779808cbbac279c199ddd3b4864bdd1ecd
jip verhoeven <jipverhoeven25@outlook.com>
2024-11-12 09:57:40 +0100

    vacature pagina

---
+++ vacature.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vacature</title>
+</head>
+<body>
+    <form>
+        <h3>Bedrijfsnaam</h3>
+        <input></input>
+        <h3>Locatie</h3>
+        <input></input>
+        <h3>Informatie bedrijf</h3>
+        <input></input>
+        <h3>Contact gegevens</h3>
+        <input></input>
+        <input type="submit"></input>
+        
+    </form>
+</body>
+</html>
