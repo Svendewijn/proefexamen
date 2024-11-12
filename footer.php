d7b0908e960854be7f75ef00cd6a6974b73ab9c7
Jack Kragten <gatoz11.com@gmail.com>
2024-11-12 15:24:04 +0100

    super belangrijke commit

--- footer.php
+++ footer.php
@@ -1,3 +1,37 @@
+<script>
+    const boemImage = document.getElementById('boemImage');
+    const boemSound = document.getElementById('boemSound');
+    let typedSequence = '';
+    const targetSequence = 'boem';
+
+    document.addEventListener('keydown', function(event) {
+        // Voeg de ingedrukte toets toe aan de sequence
+        typedSequence += event.key;
+
+        // Controleer of de sequence overeenkomt met de doelsequence
+        if (typedSequence === targetSequence) {
+            // Toont de afbeelding
+            boemImage.style.display = 'block';
+            // Speel het geluid af
+            boemSound.play();
+                
+            // Verberg de afbeelding na 2 seconden
+            setTimeout(function() {
+                boemImage.style.display = 'none';
+            }, 2000);
+
+            // Reset de sequence
+            typedSequence = '';
+        }
+
+        // Reset de sequence als deze niet overeenkomt met de doelsequence
+        if (!targetSequence.startsWith(typedSequence)) {
+            typedSequence = '';
+        }
+    });
+</script>
+
+
 <div class="footer">
     <div class="company-links">
     <h3>Email: xxl@company.com</h3>
--- index.php
+++ index.php
@@ -31,38 +31,5 @@
         <audio id="boemSound" src="images/boem.mp3"></audio>
     </div>
     <?php include 'footer.php';?>
-
-    <script>
-        const boemImage = document.getElementById('boemImage');
-        const boemSound = document.getElementById('boemSound');
-        let typedSequence = '';
-        const targetSequence = 'boem';
-
-        document.addEventListener('keydown', function(event) {
-            // Voeg de ingedrukte toets toe aan de sequence
-            typedSequence += event.key;
-
-            // Controleer of de sequence overeenkomt met de doelsequence
-            if (typedSequence === targetSequence) {
-                // Toont de afbeelding
-                boemImage.style.display = 'block';
-                // Speel het geluid af
-                boemSound.play();
-                
-                // Verberg de afbeelding na 2 seconden
-                setTimeout(function() {
-                    boemImage.style.display = 'none';
-                }, 2000);
-
-                // Reset de sequence
-                typedSequence = '';
-            }
-
-            // Reset de sequence als deze niet overeenkomt met de doelsequence
-            if (!targetSequence.startsWith(typedSequence)) {
-                typedSequence = '';
-            }
-        });
-    </script>
 </body>
 </html>
