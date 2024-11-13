63bd8bcf5ccfda6ca2b857cdf8283d644acc9e91
Jack Kragten <gatoz11.com@gmail.com>
2024-11-13 13:26:04 +0100

    nog een sql update

--- sql/gebruikers.sql
+++ sql/gebruikers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2024 at 09:56 AM
+-- Generation Time: Nov 13, 2024 at 01:25 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -41,7 +41,9 @@ CREATE TABLE `gebruikers` (
 --
 
 INSERT INTO `gebruikers` (`id`, `gebruikersnaam`, `email`, `wachtwoord`, `geregistreerd_op`, `rol`) VALUES
-(3, 'fastracist', 'racist@gmail.com', '$2y$10$SJQgEKt5bDeiVfxz68LqouO0ZPd99CPCACPjg0HbLxCcCRSBlFpve', '2024-11-12 08:48:04', 'werknemer');
+(3, 'fastracist', 'racist@gmail.com', '$2y$10$SJQgEKt5bDeiVfxz68LqouO0ZPd99CPCACPjg0HbLxCcCRSBlFpve', '2024-11-12 08:48:04', 'werknemer'),
+(4, 'jannes', 'jannes@gmail.com', '$2y$10$kp4lFFAj826G6wqae5Ukd.8IMu9SIrHcN08WM9noT0mYt6jE7ynUe', '2024-11-12 09:53:15', 'werknemer'),
+(5, 'sharkman', 'sharkman@shark.nl', '$2y$10$L8er.05XM93MBex09jh7YeePsHV7V9xfr/uYhtA24g//OT/oE7mce', '2024-11-13 10:44:57', 'werknemer');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +63,7 @@ ALTER TABLE `gebruikers`
 -- AUTO_INCREMENT for table `gebruikers`
 --
 ALTER TABLE `gebruikers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
