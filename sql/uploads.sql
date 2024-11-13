8481cab9fad83ed03d7bec158803c7f22897dddb
Jack Kragten <gatoz11.com@gmail.com>
2024-11-13 13:22:26 +0100

    sql update

--- sql/uploads.sql
+++ sql/uploads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2024 at 11:53 AM
+-- Generation Time: Nov 13, 2024 at 01:22 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
