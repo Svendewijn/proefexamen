3306d0adee18db936789cbc0e86396e480545b8a
jip verhoeven <jipverhoeven25@outlook.com>
2024-11-13 15:22:20 +0100

    sql tabel

--- sql/vacatureposts.sql
+++ sql/vacatureposts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 13 nov 2024 om 11:34
+-- Gegenereerd op: 13 nov 2024 om 15:21
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.0.30
 
@@ -42,12 +42,7 @@ CREATE TABLE `vacatureposts` (
 --
 
 INSERT INTO `vacatureposts` (`id`, `titel`, `beschrijving`, `locatie`, `salaris`, `datum_geplaatst`, `gebruiker_id`) VALUES
-(1, 'derakkersbv', 'ewa pik kom bij ons bedrijf werken hoemmo', 'Den Helder', 2.00, '2024-11-12 09:30:13', 0),
-(3, 'a', 'a', '2', 2.00, '2024-11-12 10:50:10', 0),
-(4, 'appal', 'wij zijn appal', 'appallaan 1', -0.10, '2024-11-12 12:21:13', 0),
-(5, 'appal', 'wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.wij zijn appal.', 'appallaan 1', -0.10, '2024-11-12 12:28:53', 0),
-(7, 'jip', 'jip', 'jip', -1.00, '2024-11-13 08:38:58', 4),
-(8, 'de klus', 'jij moet ff die gappie omleggen voor 20 doessoe', 'geheime locatie', 20000.00, '2024-11-13 09:45:52', 4);
+(13, 'derakkersbv', 'wij zijn de rakkers kleine tyfus aap', 'delly', 2.00, '2024-11-13 14:17:56', 6);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -67,7 +62,7 @@ ALTER TABLE `vacatureposts`
 -- AUTO_INCREMENT voor een tabel `vacatureposts`
 --
 ALTER TABLE `vacatureposts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
